724dfd53de39d9cfcd05054a3ba58afb45dc1ec0
NATHALY LOUISE LESCZK CUNHA <nathy.zulmira.com>
2025-12-04 17:00:22 -0300

    Atualização personagens.php

--- personagens.php
+++ personagens.php
@@ -5,6 +5,9 @@ require "classes/Lobisomem.php";
 require "classes/Vampiro.php";
 require "classes/Zumbi.php";
 
+$classe = "";
+
+
 if ($classe === 'Fantasma') {
      $personagem = new Fantasma();
 } else if ($classe === 'Zumbi') {
@@ -24,7 +27,7 @@ if ($classe === 'Fantasma') {
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="estilos/Style.css">
+    <link rel="stylesheet" href="./css/style.css">
 
     <style>
         .card-login {
